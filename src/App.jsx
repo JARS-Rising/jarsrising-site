9e9ede025f7fde3b4c1bcaa4a8575efd22f3725b
JARS-Rising <team@jarsrising.com>
2025-10-20 13:09:42 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -72,11 +72,18 @@ export default function App() {
         </div>
         <div className="rounded-2xl border border-zinc-200 bg-white shadow-sm p-4">
           <div className="w-full rounded-xl overflow-hidden">
-            <iframe title="Spotify Embed" style={{ borderRadius: 12 }}
-              src="https://open.spotify.com/embed/album/5ZmrKAzZpWlXYm9P44qkHX?utm_source=generator"
-              width="100%" height="352" frameBorder="0"
-              allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-              loading="lazy" />
+           <iframe
+  data-testid="embed-iframe"
+  style="border-radius:12px"
+  src="https://open.spotify.com/embed/artist/1UUPR8xdBGlduxNDM7cedg?utm_source=generator"
+  width="100%"
+  height="352"
+  frameBorder="0"
+  allowfullscreen=""
+  allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+  loading="lazy">
+</iframe>
+
           </div>
         </div>
       </section>
